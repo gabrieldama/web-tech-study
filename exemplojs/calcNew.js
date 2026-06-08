89a588872d8df93276fa87222026fa4e7c2a65a7
Gabriel Damaceno <165826063+gabrieldama@users.noreply.github.com>
2026-06-08 11:41:57 -0300

    feat: criando exemplos de funções

---
+++ exemplojs/calcNew.js
@@ -0,0 +1,5 @@
+function meuNome(nome) {
+    console.log(nome);
+}
+meuNome("Gabriel");
+meuNome("Maria");
---
+++ exemplojs/exercicio2.js
@@ -0,0 +1,15 @@
+// ======= EXEMPLO DO EXERCIOS =======
+
+// const javaScript = "Java Script";
+
+// const js = javaScript[0] + javaScript[5];
+
+// console.log(js); 
+
+const mozilla = "Mozilla Developer Network";
+
+// mdn
+
+const mdn = mozilla[0] + mozilla[8] + mozilla[18];
+
+console.log(mdn);
