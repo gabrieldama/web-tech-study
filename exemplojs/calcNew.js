72222a4cda5869b42101e361e91c377452b0fcf6
Gabriel Damaceno <165826063+gabrieldama@users.noreply.github.com>
2026-06-08 15:52:11 -0300

    feat: adicionando operações aritméticas

--- exemplojs/calcNew.js
+++ exemplojs/calcNew.js
@@ -3,6 +3,20 @@ function soma(num1, num2) {
     console.log("Soma:", num1 + num2);
 }
 
+function subtracao(num1, num2) {
+    console.log("Subtração:", num1 - num2);
+}
+
+function multiplicacao(num1, num2) {
+    console.log("Multiplicação:", num1 * num2);
+}
+
+function divisao(num1, num2) {
+    console.log("Divisão:", num1 / num2);
+}
+
 // Imprimir resultados
 soma(10, 10);
 soma(20, 47);
+subtracao(10, 5);
+multiplicacao(3, 4);
