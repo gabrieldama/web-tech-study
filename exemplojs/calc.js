61d87008f3ae039702376729b634cd64cc4412d3
Gabriel Damaceno <165826063+gabrieldama@users.noreply.github.com>
2026-06-08 11:22:24 -0300

    feat: calculos e exercicio

---
+++ exemplojs/calc.js
@@ -0,0 +1,19 @@
+let num1 = 10;
+let num2 = 2;
+
+// Soma
+let soma = num1 + num2;
+console.log("Soma:", soma);
+
+// Subtração
+let subtracao = num1 - num2;
+console.log("Subtração:", subtracao);   
+
+// Multiplicação
+let multiplicacao = num1 * num2;
+console.log("Multiplicação:", multiplicacao);
+
+// Divisão
+let divisao = num1 / num2;
+console.log("Divisão:", divisao);
+
