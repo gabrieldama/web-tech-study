b3ad9ea2ba14f13c52036eccbb083e31c1adf305
Gabriel Damaceno <165826063+gabrieldama@users.noreply.github.com>
2026-06-08 10:46:06 -0300

    feat: adicionando codigos novos

---
+++ exemplojs/tipo-dados.js
@@ -0,0 +1,18 @@
+// String
+let nome = "Gabriel";
+
+// Inteiro
+let idade = 21;
+
+//Booleano
+let Casado = false;
+
+// Numeros flutuantes com casas decimais
+let valorProduto = 19.99;
+
+// Resultados
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Casado:", Casado);
+console.log("Valor do Produto:", valorProduto);
+console.log(typeof nome); // string
