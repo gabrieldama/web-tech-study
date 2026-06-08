d2adcfc8726408378716ed5853df2a31b834a86d
Gabriel Damaceno <165826063+gabrieldama@users.noreply.github.com>
2026-06-08 11:49:36 -0300

    feat: adicionando funções em js

--- exemplojs/calcNew.js
+++ exemplojs/calcNew.js
@@ -1,5 +1,8 @@
-function meuNome(nome) {
-    console.log(nome);
+// Funções
+function soma(num1, num2) {
+    console.log("Soma:", num1 + num2);
 }
-meuNome("Gabriel");
-meuNome("Maria");
+
+// Imprimir resultados
+soma(10, 10);
+soma(20, 47);
