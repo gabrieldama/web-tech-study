e423c4f77be74a906c742e2a7cd832c1ed8a700a
Gabriel Damaceno <165826063+gabrieldama@users.noreply.github.com>
2026-06-15 08:34:09 -0300

    feat: adicionando validação de nota e display de mensagens

--- nota-aluno/js/nota.js
+++ nota-aluno/js/nota.js
@@ -1,28 +1,31 @@
 // Variaveis
 const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("resultado");
+const message = document.getElementById("message");
 
 function resultadoNotaClick(event) {
     event.preventDefault();
     const notaValue = inputNota.value;
     // Validação
     if (notaValue == "") {
-        alert("Por favor, digite a nota do aluno.");
-        return false;
-    }
+        message.style.display = "block";
+    } else {
+        message.style.display = "none";
 
     if (notaValue < 0 || notaValue > 10) {
         alert("Por favor, digite uma nota válida entre 0 e 10.");
-        return false;
+        return;
     }
 // Logica
     if (notaValue >= 6) {
-    alert("Aluno aprovado");
+    resultado.innerHTML = "Aluno aprovado";
 } else {
-    alert("Aluno reprovado");
-}
+    resultado.innerHTML = "Aluno reprovado";
 }
 
+inputNota.value = "";
+    }
+}
 
 
 
