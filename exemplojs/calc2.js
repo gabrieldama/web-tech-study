456244c7f2c7b3823df650455e7362d8b47839f9
Gabriel Damaceno <165826063+gabrieldama@users.noreply.github.com>
2026-06-15 08:36:23 -0300

    feat: adicionando arquivo calc2.js com operações aritméticas e index.html

---
+++ exemplojs/calc2.js
@@ -0,0 +1,20 @@
+let num1 = Number(prompt("Digite o primeiro número:"));
+let num2 = Number(prompt("Digite o segundo número:"));
+// Number() converte a string para número
+
+
+// Operações
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+
+console.log(typeof soma);
+console.log(typeof subtracao);
+console.log(typeof multiplicacao);
+console.log(typeof divisao);
