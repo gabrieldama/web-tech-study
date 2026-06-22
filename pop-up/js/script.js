afacbf6462cff494f1ef818abdc4ad8ccff79612
Gabriel Damaceno <165826063+gabrieldama@users.noreply.github.com>
2026-06-22 08:20:37 -0300

    fix: arrumando script.js

--- pop-up/js/script.js
+++ pop-up/js/script.js
@@ -4,14 +4,11 @@ const popup = document.querySelector(".popup-wrapper")
 button.addEventListener("click", function(event) {
     popup.classList.add("d-block")
 })
-
+// O objetivo é fazer com que, ao clicar nos elementos dentro do pop-up, ele mostre o popup
 popup.addEventListener("click", function(event) {
-    const classOfClickElement = event.target.classList[0]
-    const classNameListArray = ["popup-close", "popup-link", "popup-wrapper"]
-
-    const isClassList = (classNameListArray.includes(classOfClickElement))
-    
-    if (isClassList) {
+    if (event.target.classList.contains("popup-close") ||
+        event.target.classList.contains("popup-link") ||
+        event.target.classList.contains("popup-wrapper")) {
         popup.classList.remove("d-block")
     }
 })
