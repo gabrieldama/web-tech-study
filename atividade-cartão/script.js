12b7cc92ff1b5579395b5a787b054c274217837b
Gabriel Damaceno <165826063+gabrieldama@users.noreply.github.com>
2026-08-05 11:25:46 -0300

    feat: adicionando atividade de cartão interativo. utilizando HTML, CSS e JavaScript

---
+++ atividade-cartão/script.js
@@ -0,0 +1,62 @@
+const botao = document.querySelector("#btnMudar")
+// botão que reverte o estilo do card para o original
+const botaoReverter = document.querySelector("#btnReverter")
+const botaoSurpresa = document.querySelector("#btnSurpresa")
+const card = document.querySelector("#card")
+const texto = document.querySelector("#texto")
+const titulo = document.querySelector("#titulo")
+const descricao = document.querySelector("#descricao")
+
+botao.addEventListener("click", function() {
+botao.style.backgroundColor = "red";
+botao.style.display = "none";
+
+card.style.backgroundColor = "#333533";
+
+texto.style.color = "white";
+texto.innerHTML = "Funcionou, o card mudou de cor e o botão sumiu!";
+
+titulo.innerText = "Card Alterado com sucesso!";
+titulo.style.color = "white";
+
+descricao.style.display = "none";
+
+botaoReverter.style.display = "block";
+botaoSurpresa.style.display = "block";
+
+//Deixa o background da pagina gradiente
+document.body.style.background = "linear-gradient(to right, #6b6a69, #636160)";
+
+//Adiciona uma imagem no background da pagina
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundPosition = "center";
+})
+
+botaoReverter.addEventListener("click", function() {
+botao.style.backgroundColor = "#007bff";
+botao.style.display = "block";
+
+card.style.backgroundColor = "#f8f9fa";
+
+texto.style.color = "black";
+titulo.style.color = "black";
+descricao.style.color = "black";
+
+botaoReverter.style.display = "none";
+
+document.body.style.background = "linear-gradient(to right, #f8f9fa, #e9ecef)";
+});
+
+botaoSurpresa.addEventListener("click", function() {
+    
+document.body.style.backgroundImage = "url('https://i.pinimg.com/736x/77/c1/33/77c13333446751de8bb1d8d352661ee4.jpg')";
+botao.style.backgroundColor = "purple";
+botao.style.display = "none";
+
+botaoReverter.style.display = "none";
+botaoSurpresa.style.display = "none";
+
+texto.style.display = "none";
+descricao.style.display = "none";
+titulo.innerText = "hmmm bolo de mulangu";
+});
