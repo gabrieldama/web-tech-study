e3ce31b186453c83be4a7b7c05e6e5560ab8108c
Gabriel Damaceno <165826063+gabrieldama@users.noreply.github.com>
2026-08-12 10:18:25 -0300

    feat: adicionando projetos em React e criando Header

---
+++ introducao-react/src/App.jsx
@@ -0,0 +1,28 @@
+import Header from "./Components/Header"
+
+function App() {
+  return (
+    <>
+     <Header/>
+
+     <section>
+      <article>
+        <h1>Lorem Ipsun</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, accusantium. Nihil suscipit amet consequatur est, sapiente sunt voluptatibus ratione dolorem, ea aliquid earum. Excepturi similique quo tenetur maiores ipsum enim.</p>
+      </article>
+
+      <article>
+        <h1>Lorem Ipsun</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, accusantium. Nihil suscipit amet consequatur est, sapiente sunt voluptatibus ratione dolorem, ea aliquid earum. Excepturi similique quo tenetur maiores ipsum enim.</p>
+      </article>
+      <hr />
+     </section>
+
+     <footer>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. </p>
+     </footer>
+    </>
+  )
+}
+
+export default App
