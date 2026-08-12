55c9982bde3909b6525714da0fa862db9f5d1bcb
Gabriel Damaceno <165826063+gabrieldama@users.noreply.github.com>
2026-08-12 10:51:25 -0300

    feat: adicionando card e estilização CSS

---
+++ introducao-react/src/Components/Card.jsx
@@ -0,0 +1,19 @@
+import "./Card.css"
+
+// Import da imagem
+import imgCard from "../assets/mg-card.jpg"
+
+const Card = () => {
+    return (
+    <section>
+      <article className="card">
+        <img src={imgCard} className="img-card"/>
+        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, accusantium. Nihil suscipit amet consequatur est, sapiente sunt voluptatibus ratione dolorem, ea aliquid earum. Excepturi similique quo tenetur maiores ipsum enim.</p>
+    </article>
+
+      <hr />
+     </section>
+    )
+}
+
+export default Card;
