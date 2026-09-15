a2c34db0343f5963d1d847092cc304022f13ad6c
GABRIEL TEIXEIRA ALVES DAMACENO <gabriel.damaceno4@senaisp.edu.br>
2026-09-15 14:36:40 -0300

    feat: adicionando texto e css no Sobre e FAQ

--- projeto-react/src/Pages/Sobre/index.jsx
+++ projeto-react/src/Pages/Sobre/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import "./Sobre.css"
 
 function index() {
   return (
     <section className="about-container">
-        <h1 className="about-title">Sobre</h1>
-        <p className="about-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+      <h1 className="about-title">Sobre</h1>
+
+      <p className="about-text">
+        Este é o meu segundo projeto em React, desenvolvido durante o curso no SENAI.
+        A proposta foi aprofundar os conhecimentos em desenvolvimento web e aprender a
+        construir interfaces mais organizadas, reutilizáveis e responsivas.
+      </p>
+
+      <p className="about-text">
+        Durante o processo, foi possível praticar a criação de componentes, a estruturação
+        de páginas, o uso de estilos e a navegação entre telas. Também foi importante
+        entender como o React ajuda a organizar melhor o código e a facilitar a manutenção
+        de aplicações maiores.
+      </p>
+
     </section>
   )
 }
